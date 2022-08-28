8d94e49f5d3069363d01ed96389c79b6a55b0249
nicodev-co <nicodev26@gmail.com>
2022-08-27 23:19:16 -0500

    Ejemplo de promesas

---
+++ src/promise/index.js
@@ -0,0 +1,19 @@
+const promise = new Promise(function (resolve, reject) {
+    resolve('hey!');
+});
+
+const cows = 9;
+
+const countCows = new Promise(function (resolve, reject) {
+    if (cows > 10) {
+        resolve(`We have ${cows} cows on the farm`);
+    } else {
+        reject('There is no ccows on the farm');
+    }
+});
+
+countCows.then((result) => {
+    console.log(result);
+}).catch((error) => { 
+    console.log(error);
+}).finally(() => console.log('Finally'));
