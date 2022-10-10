76855095a1a5869d274a87b4a63477b3238385da
nicodev-co <nicodev26@gmail.com>
2022-10-09 22:06:53 -0500

    Variables de entorno Dotenv Webpack

--- webpack/portfolio/webpack.config.js
+++ webpack/portfolio/webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: "./src/index.js",
@@ -68,6 +69,7 @@ module.exports = {
         }
       ]
     }),
+    new Dotenv(),
   ],
   optimization: {
     minimize: true,
