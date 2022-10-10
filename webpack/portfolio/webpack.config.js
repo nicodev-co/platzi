7b513f3adaea5bb71149d7b6dd769892dc749e84
nicodev-co <nicodev26@gmail.com>
2022-10-09 21:51:35 -0500

    Alias webpack

--- webpack/portfolio/webpack.config.js
+++ webpack/portfolio/webpack.config.js
@@ -14,6 +14,12 @@ module.exports = {
   },
   resolve: {
     extensions: [".js"],
+    alias: {
+      '@utils': path.resolve(__dirname,'src/utils'),
+      '@templates': path.resolve(__dirname,'src/templates'),
+      '@styles': path.resolve(__dirname,'src/styles'),
+      '@images': path.resolve(__dirname,'src/assets/images/'),
+    }
   },
   module: {
     rules: [
