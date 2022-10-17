3a691bb80909bcac8146d067fff4422a0bd61ded
nicodev-co <nicodev26@gmail.com>
2022-10-16 19:29:15 -0500

    server local de desarrollo

--- webpack/portfolio/webpack.config.dev.js
+++ webpack/portfolio/webpack.config.dev.js
@@ -69,5 +69,11 @@ module.exports = {
       ]
     }),
     new Dotenv(),
-  ]
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+  },
 };
