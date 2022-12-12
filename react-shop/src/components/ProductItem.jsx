2917d051aca0d76829587d33d4989e2a5d09f689
nicodev <nicodev@pop-os.localdomain>
2022-12-11 22:37:02 -0500

    listar productos desde api con axios

--- react-shop/src/components/ProductItem.jsx
+++ react-shop/src/components/ProductItem.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React,{useState} from "react";
 import "@styles/ProductItem.scss";
 import addCart from "@icons/bt_add_to_cart.svg";
 
 const ProductItem = () => {
+  const [cart, setCart] = useState([]);
+  const handleClick = () => {
+    setCart([]);
+  }
+
   return (
     <div className="ProductItem">
       <img
@@ -14,7 +19,7 @@ const ProductItem = () => {
           <p>$120,00</p>
           <p>Bike</p>
         </div>
-        <figure>
+        <figure onClick={handleClick}>
           <img src={addCart} alt="" />
         </figure>
       </div>
