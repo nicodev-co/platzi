f1ea11baeb13c2366a680c3062438cac1a8ee0ed
nicodev-co <nicodev26@gmail.com>
2023-09-10 16:06:04 -0500

    Add curso react intro

---
+++ curso-react-intro/src/CreateTodoButton.js
@@ -0,0 +1,7 @@
+function CreateTodoButton() {
+    return (
+        <button>Agregar</button>
+    );
+}
+
+export { CreateTodoButton }
---
+++ curso-react-intro/src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem() {
+    return (
+        <li>
+            <span>V</span>
+            <p>Llorar por la llorona</p>
+            <span>X</span>
+        </li>
+    );
+}
+
+export { TodoItem }
