acd06a09d02294619596d55aa407a7fef852dcc3
nicodev-co <nicodev26@gmail.com>
2023-09-10 16:33:55 -0500

    clase 4 props y atributos

--- curso-react-intro/src/App.js
+++ curso-react-intro/src/App.js
@@ -4,23 +4,33 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
+import React from "react";
+
+const defaultTodos = [
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tomar el curso de intro a React.js", completed: false },
+  { text: "LLorar con la llorona", completed: false },
+  { text: "LALALALA", completed: true }
+];
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
 
-      <TodoCounter />
+      <TodoCounter completed={16} total={25} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        ))}
+
       </TodoList>
 
       <CreateTodoButton />
 
-    </div>
+    </React.Fragment>
+
   );
 }
 
